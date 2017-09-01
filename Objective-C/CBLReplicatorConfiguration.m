b08d52b0b6de6414aff14779f876b249261de33b
Pasin Suriyentrakorn <pasin@couchbase.com>
2017-08-31 18:51:19 -0700

    Implement replicator backgrounding option
    * Add runInBackground option to the ReplicatorConfiguration. The default value is NO.
    
    * When runInBackground is not enabled, when the app goes into the background, the replicator will be suspended or stopped if the replicator is not continuous after it becomes inactive. The replicator will resume when the app is brought back to foreground again. The replicator can be also resumed when the -start method is called. This allows the replicator to be started or worked when the app is already in the background.
    
    * When runInBackground is enabled, the replicator will continue to run in the background, and it’s application’s responsibility to manage the replicator and background task.
    
    * Created a new serial queue for replicator instead of using main queue; the notification is still posted on the main queue.
    
    * Made start / stop running inside the replicator dispatch queue.
    
    * MYBackgroundMonitor is not compiled on XCode9; wait for https://github.com/snej/MYUtilities/pull/16 to be merged.

--- Objective-C/CBLReplicatorConfiguration.m
+++ Objective-C/CBLReplicatorConfiguration.m
@@ -26,6 +26,9 @@
 @synthesize authenticator=_authenticator, documentIDs=_documentIDs, channels=_channels;
 @synthesize checkpointInterval=_checkpointInterval;
 
+#if TARGET_OS_IPHONE
+@synthesize runInBackground=_runInBackground;
+#endif
 
 + (instancetype) withDatabase: (CBLDatabase*)database targetURL: (NSURL*)targetURL {
     return [[self alloc] initWithDatabase: database targetURL: targetURL];
@@ -71,6 +74,10 @@
     c.documentIDs = _documentIDs;
     c.channels = _channels;
     c.checkpointInterval = _checkpointInterval;
+    
+#if TARGET_OS_IPHONE
+    c.runInBackground = _runInBackground;
+#endif
     return c;
 }
 
